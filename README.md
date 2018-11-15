cba96a568756e75431f7f24774263397fae68a05
vjami <vjami@paypal.com>
2018-11-16 02:18:48 +0530

    User register and login Page

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ https://www.tutorialspoint.com/nodejs/nodejs_restful_api.htm
 
 https://hackernoon.com/the-practical-guide-for-building-rest-api-in-nodejs-and-mongodb-include-passport-and-jwt-476720b70da0
 
+https://appdividend.com/2018/07/18/react-redux-node-mongodb-jwt-authentication/
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+const users = require('./app/routes/users'); 
 
 // create express app
 const app = express();
+
+app.use(passport.initialize());
+require('./app/passport')(passport);
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -30,9 +36,13 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to my first node  application."});
 });
 
+app.use('/api/users', users);
+
 require('./app/routes/note.routes.js')(app);
 
+const PORT = process.env.PORT || 5000;
+
 // listen for requests
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3000");
 });
